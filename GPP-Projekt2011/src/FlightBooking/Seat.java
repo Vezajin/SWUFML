f5734cf132605ebfc00b8c0191912ada0a4b7182
Nikolaj <nikolajzl@hotmail.com>
2012-11-27 17:34:43 +0100

    11th commit

--- GPP-Projekt2011/src/FlightBooking/Seat.java
+++ GPP-Projekt2011/src/FlightBooking/Seat.java
@@ -1,10 +1,50 @@
 package FlightBooking;
 
+import java.sql.*;
 /**
  *
  * @author Nikolaj
  */
 public class Seat {
     private int key, flightid, customerid;
+    
+    public Seat(int k, int f, int c) {
+        key = 0;
+        flightid = f;
+        customerid = c;
+    }
+    
+    public void init(ResultSet rs) throws SQLException {
+        key = rs.getInt("id");
+        flightid = rs.getInt("flightid");
+        customerid = rs.getInt("customerid");
+    }
+    
+    public Seat(Database db, int k) throws SQLException {
+        ResultSet rs = db.execute("SELECT * FROM Seats WHERE id = " + k);
+        rs.next();
+        init(rs);
+    }
+    
+    public void insert(Database db) throws SQLException {
+        db.execute ("INSERT INTO Seats (flightid, customerid) VALUES ('" + flightid + "', '" + customerid + "')");
+        ResultSet rs = db.execute("SELECT MAX(id) as 'max' FROM Seats");
+        rs.next();
+        key = rs.getInt("max");
+    }
+    
+    public int getKey() {
+        return key;
+    }
+    
+    public int getFlightid() {
+        return flightid;
+    }
+    
+    public int getCustomerid() {
+        return customerid;
+    }
+    
         
+    
 }
---
+++ GPP-Projekt2011/src/FlightBooking/TestClass.java
@@ -0,0 +1,11 @@
+package FlightBooking;
+
+/**
+ * Write test related stuff in here GUYS!?!?
+ * @author Nikolaj
+ */
+public class TestClass {
+    
+    public TestClass() {
+    }
+}
