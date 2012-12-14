16d99f4cf96bb3b5e7eef0bc978234985aa307b6
Frederik <Frederikhoffmann1@gmail.com>
2012-12-14 15:33:59 +0100

    76th commit

--- GPP-Projekt2011/src/FlightBooking/FlightScanner.java
+++ GPP-Projekt2011/src/FlightBooking/FlightScanner.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 
 /**
  * This is the class that scans Strings retrieved from the database. These 
- * Strings contain names (first name and last name) and seats per order. 
+ * Strings contain names (first name and last name), seats per order and
+ * destinations. 
  * @author FrederikTH
  */
 public class FlightScanner {
@@ -19,6 +20,7 @@ public class FlightScanner {
      * seats in a given order (seats only). 
      * @return Returns an ArrayList comprising of Strings where each index equals 
      * a seat which is booked. 
+     * Uses whitespace as a delimiter. This is standard
      */
     public ArrayList<String> seatAnalyser(String input) {
         Scanner scan = new Scanner(input);
@@ -38,7 +40,8 @@ public class FlightScanner {
      * @param input Is a String retrieved from the database. It contains the 
      * all first names and last names in a given order. 
      * @return Returns an ArrayList comprising of Strings where each index equals 
-     * a seat which is booked. 
+     * a customers name. 
+     * Uses a comma "," as a delimiter. 
      */   
     public ArrayList<String> nameAnalyser (String input) {
         Scanner scan = new Scanner(input);
@@ -46,19 +49,28 @@ public class FlightScanner {
             // Returns true if the String has a token. If false the String is empty.
         scan.useDelimiter(",");    
         while(scan.hasNext()==true) {               
-                // Returns true if the String has a token. If false the String is empty.                
+            // Scans the token and adds it to the arraylist.               
                 nameScan.add(scan.next());               
             }
         return nameScan;
     }
     
+      /**
+     * This method scans and breaks String input into Strings and stores 
+     * them in an Array. Only usable with destination-Strings.
+     * @param input Is a String retrieved from the database. It contains the 
+     * the start and end destination of a given flight. 
+     * @return Returns an ArrayList comprising of Strings where each index equals 
+     * a seat which is booked. 
+     * Uses a dash "-" as a delimtier. 
+     */  
     public ArrayList<String> destinationAnalyser (String input) {
         Scanner scan = new Scanner(input);
         ArrayList<String> desScan = new ArrayList<String>();  
             // Returns true if the String has a token. If false the String is empty.
         scan.useDelimiter("-");    
         while(scan.hasNext()==true) {               
-                // Returns true if the String has a token. If false the String is empty.                
+            // Scans the token and adds it to the arraylist.            
                 desScan.add(scan.next());               
             }
         return desScan;
